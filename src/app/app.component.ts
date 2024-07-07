1147e50a1c11d4d04ea6b5560f0640f587506a28
Rodion Sibov <rodionsibov@gmail.com>
2024-07-07 11:28:26 +0300

    Managing todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserService } from './user.service';
+import { ToDo, TodoService } from './todo.service';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,28 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'state-signals';
+ // Services
+ userService = inject(UserService);
+ todoService = inject(TodoService);
+
+ // Signals  
+ users = this.userService.members;
+ isLoading = this.todoService.isLoading;
+ currentMember = this.todoService.currentMember;
+ todosForMember = this.todoService.filteredToDos;
+ errorMessage = this.todoService.errorMessage;
+
+ // Actions
+ onFilter(ele:EventTarget | null) {
+   this.todoService.filterToDos((ele as HTMLInputElement).checked)
+ }
+
+ onSelected(ele:EventTarget | null) {
+   this.todoService.getToDosForMember(Number((ele as HTMLSelectElement).value));
+ }
+
+ onChangeStatus(task: ToDo, ele: EventTarget | null) {
+   this.todoService.changeStatus(task, (ele as HTMLInputElement).checked);
+ }
+
 }
